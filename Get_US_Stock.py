99c2c126a93033c241e4d38f9128df1e86997fd7
stealthydonut <kinger6688@gmail.com>
2017-05-31 14:40:48 -0400

    Update Get_US_Stock.py

--- Get_US_Stock.py
+++ Get_US_Stock.py
@@ -1,6 +1,8 @@
 import urllib
 import pandas as pd
+import html5lib
 
 dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
 urllib.urlretrieve(dls, "test.xls")
 data = pd.read_html('test.xls')
+f_states=   pd.read_html('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download') 
