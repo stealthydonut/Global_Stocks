65544abbd22f78f357dd8bf18ef275dd23878b20
stealthydonut <kinger6688@gmail.com>
2017-05-31 14:38:26 -0400

    Create Get_US_Stock.py

---
+++ Get_US_Stock.py
@@ -0,0 +1,6 @@
+import urllib
+import pandas as pd
+
+dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
+urllib.urlretrieve(dls, "test.xls")
+data = pd.read_html('test.xls')
