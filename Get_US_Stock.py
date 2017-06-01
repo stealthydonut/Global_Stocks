9e1e646193b43549a37f5f466730f4d29ac91fc1
stealthydonut <kinger6688@gmail.com>
2017-06-01 16:21:17 -0400

    Update Get_US_Stock.py

--- Get_US_Stock.py
+++ Get_US_Stock.py
@@ -10,22 +10,55 @@ import html5lib
 import beautifulsoup4
 import datetime
 
+######################################
+#Download the files from the exchanges
+######################################
+
 dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
 urllib.urlretrieve(dls, "test.xls")
-data = pd.read_csv('test.xls')
+data1 = pd.read_csv('test.xls')
+data1['Index']='nasdaq'
 
 dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
 urllib.urlretrieve(dls, "test.xls")
-data = pd.read_csv('test.xls')
+data2 = pd.read_csv('test.xls')
+data2['Index']='nyse'
 
 dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download"
 urllib.urlretrieve(dls, "test.xls")
-data = pd.read_csv('test.xls')
+data3 = pd.read_csv('test.xls')
+data3['Index']='amex'
 
+###################################
+#Set the data on top of one another
+###################################
+set1 = data1.append(data2, ignore_index=True)
+ticker = data3.append(set1, ignore_index=True) 
+
+###############################################
 #Create a timestamp to insert on a master table
+###############################################
 
 from datetime import datetime, timedelta
-timevalue = datetime.today() - timedelta(days=1)
-day = timevalue.day
-month = timevalue.month
-year = timevalue.year
+insert = datetime.today()
+
+ticker['insert date'] = datetime.today()
+
+##################################
+#Clean up the market captilization 
+##################################
+#ticker['char']=ticker['MarketCap'].str[-1:]
+#tickerB = ticker['char'] == 'B'
+
+##################################
+#Export the file to google storage
+##################################
+
+bucket2 = client.get_bucket('mastfiles')
+df_out = pd.DataFrame(ticker)
+df_out.to_csv('ticker_listUS.csv', index=False)
+blob2 = bucket2.blob('ticker_listUS.csv')
+blob2.upload_from_filename('ticker_listUS.csv')
+
+
+
