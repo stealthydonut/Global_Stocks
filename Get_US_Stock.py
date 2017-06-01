78a0662a2fabf91db42fea3af38ceee6c9d4bd4f
stealthydonut <kinger6688@gmail.com>
2017-06-01 10:55:56 -0400

    Update Get_US_Stock.py

--- Get_US_Stock.py
+++ Get_US_Stock.py
@@ -1,8 +1,22 @@
+#The objective of this script is to identify the tickers on the major exchanges
+#These datasets will be used for data extraction purposes
+
 import urllib
 import pandas as pd
+#sudo -H pip install six==1.10.0
+#sudo -H pip install html5lib==1.0b8
+#sudo pip install beautifulsoup4
 import html5lib
+import beautifulsoup4
 
 dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download"
 urllib.urlretrieve(dls, "test.xls")
-data = pd.read_html('test.xls')
-f_states=   pd.read_html('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download') 
+data = pd.read_csv('test.xls')
+
+dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
+urllib.urlretrieve(dls, "test.xls")
+data = pd.read_csv('test.xls')
+
+dls = "http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download"
+urllib.urlretrieve(dls, "test.xls")
+data = pd.read_csv('test.xls')
