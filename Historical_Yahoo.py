52957a2be5b4da2b45472bd433309747921d60b7
stealthydonut <kinger6688@gmail.com>
2017-06-04 16:53:51 -0400

    Create Historical_Yahoo.py

---
+++ Historical_Yahoo.py
@@ -0,0 +1,8 @@
+
+#Yahoo ticker list
+
+https://query1.finance.yahoo.com/v7/finance/download/CCO.TO?period=1493819016&period2=1496497416&interval=1d&events=history&crumb=RkgV3tFpvN8
+
+Jan 12 - Jun 2
+
+https://query1.finance.yahoo.com/v7/finance/download/CCO.TO?period=893819016&period2=1496497412&interval=1d&events=history&crumb=RkgV3tFpvN8
