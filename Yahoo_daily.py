e77effdac5362ad0ea9f81ad638b05eab58c2201
stealthydonut <kinger6688@gmail.com>
2017-06-27 16:33:30 -0400

    Yahoo Daily File Generation

---
+++ Yahoo_daily.py
@@ -0,0 +1,52 @@
+#This program will get the historical data for a number of tickers
+import urllib
+import pandas as pd
+import numpy as np
+import StringIO
+import datetime
+import sys
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO as stio
+else:
+    from io import StringIO as stio
+
+#Get the data from google cloud storage
+from google.cloud import storage
+client = storage.Client()
+bucket = client.get_bucket('mastfiles')
+# Then do other things...
+blob = bucket.get_blob('ticker_listUS.csv')
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+df=pd.read_csv(inMemoryFile, low_memory=False)
+df1=df['Symbol']
+df2=df1.values.T.tolist()
+#strip out leading and trailing 0's
+df2 = [x.strip(' ') for x in df2]   
+
+myfile = ''
+bigdata = pd.DataFrame()
+
+myfile = ''
+for i in df2:
+    try:#Develop the text string that can get all the data
+        start="http://finance.yahoo.com/d/quotes.csv?s="
+        #date,Float Shares,Day's Low,Day's High,Open,Previous Close,Change,Volume,Name,Ticker,52 Low, 52 High,Dividend Share
+        #end="&f=d1f6ghopc1vns"
+        #date,Float ,Name,Ticker
+        end="&f=d1f6s7oc1pghnsjkdk5j6r"
+        str1 = ''.join([i])
+        text2=start+str1+end    
+        #Get the data from the yahoo api
+        link=text2
+        f = urllib.urlopen(link)
+        myfile += f.readline()
+    except:
+        print i
+     
