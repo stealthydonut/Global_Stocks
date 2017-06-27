cc1ad055fc18c7ab156e06b28087b7954fc028fe
stealthydonut <kinger6688@gmail.com>
2017-06-27 15:00:23 -0400

    Create Oil Data Pull

---
+++ Oil.py
@@ -0,0 +1,37 @@
+import urllib
+import pandas as pd
+import StringIO
+import datetime
+import sys
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO as stio
+else:
+    from io import StringIO as stio
+    
+ticker = ['APC','BTE']    
+    
+myfile = ''
+for i in ticker:
+    try:#Develop the text string that can get all the data
+        start="http://finance.yahoo.com/d/quotes.csv?s="
+        #date,Float Shares,Day's Low,Day's High,Open,Previous Close,Change,Volume,Name,Ticker
+        #end="&f=d1f6ghopc1vns"
+        #date,Float ,Name,Ticker
+        end="&f=d1f6s7oc1pghns"
+        str1 = ''.join([i])
+        text2=start+str1+end    
+        #Get the data from the yahoo api
+        link=text2
+        f = urllib.urlopen(link)
+        myfile += f.readline()
+    except:
+        print i
+     
+
+    
+TESTDATA=stio(myfile)
+
+daily_prices = pd.read_csv(TESTDATA, sep=",", names=['date','Float Shares','Short Ratio','Open','Change','Previous Close','Low','High','Name','Ticker'])
+
+
+    
