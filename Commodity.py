2f691a3e903db06382a02780e9f144673ff9eb29
stealthydonut <kinger6688@gmail.com>
2017-06-30 13:42:58 -0400

    Update Commodity.py

--- Commodity.py
+++ Commodity.py
@@ -11,6 +11,9 @@ silver = quandl.get("LBMA/SILVER")
 oil = quandl.get("OPEC/ORB")
 uranium = quandl.get("ODA/PURAN_USD")
 tax = quandl.get("FMSTREAS/MTS")
+#S&P500
+sp500 = pd.read_csv('C:\Users\davking\Downloads\^GSPC.csv')
+sp500['date2'] =  pd.to_datetime(sp500['Date'])
 
 #Index Generation
 gold['ind']=gold.index
@@ -29,6 +32,13 @@ pfile['go_ratio']=pfile['USD (PM)']/pfile['Value']
 #data manipulation to get a month and year variaable
 ism['index1'] = ism.index
 ism['monthyear'] = ism['index1'].dt.strftime("%m,%y")
+uranium['index1'] = uranium.index
+uranium['monthyear'] = uranium['index1'].dt.strftime("%m,%y")
+uranium['uranium price'] = uranium['Value']
+uranium.__delitem__('Value')
+tax['index1'] = tax.index
+tax['monthyear'] = tax['index1'].dt.strftime("%m,%y")
+
 gold['index1'] = gold.index
 gold['monthyear'] = gold['index1'].dt.strftime("%m,%y")
 gold['daycnt'] = 1
@@ -36,8 +46,28 @@ goldmth= gold.groupby(['monthyear'], as_index=False)['USD (PM)','daycnt'].sum()
 goldmth['gold_price']=goldmth['USD (PM)']/goldmth['daycnt']
 
 
-test=goldmth.merge(ism, on='monthyear', how='outer')
-print test
+sp500['monthyear'] = sp500['date2'].dt.strftime("%m,%y")
+sp500['daycnt'] = 1
+sp500mth= sp500.groupby(['monthyear'], as_index=False)['Adj Close','daycnt'].sum()
+sp500mth['spval']=sp500mth['Adj Close']/sp500mth['daycnt']
+
+test2=tax.merge(sp500mth, on='monthyear', how='outer')
+#makes a new dataframe copy so that it does not refer back to original
+test3=pd.DataFrame(test2[test2['index1'].notnull()])
+test4=pd.DataFrame(test3[test3['spval'].notnull()])
+test4['dateplotx'] = [dt.datetime(year=int(d.year), month=int(d.month), day=int(d.day)) for d in test4['index1']]
+
+
+test4['year'] = test4['index1'].dt.strftime("%Y")
+test4['month'] = test4['index1'].dt.strftime("%m")
+test4['day'] = test4['index1'].dt.strftime("%d")
+test4['ma6tax'] = test4['Receipts'].rolling(window=6).mean()
+test4['ma6spval'] = test4['spval'].rolling(window=6).mean()
+
+
+
+goldmth.merge(ism, on='monthyear', how='outer')
+
 
 pfile['dateplotx'] = [dt.datetime(year=d.year, month=d.month, day=d.day) for d in pfile['ind']]
 
@@ -47,24 +77,27 @@ pfile['month'] = pfile['ind'].dt.strftime("%m")
 pfile['day'] = pfile['ind'].dt.strftime("%d")
 #This Generates a date axis on a daily basis - for monthly exclude day, and make day=1 on dateplot line
 dateplot = []
+dateplot2 = []
 graph1 = []
 for month,year,day in zip(pfile['month'], pfile['year'], pfile['day']):
         dateplot.append(dt.datetime(year=int(year), month=int(month), day=int(day)))
-        
-        
+#build date time frame for monthly
+for month,year,day in zip(test4['month'], test4['year'], test4['day']):
+        dateplot2.append(dt.datetime(year=int(year), month=int(month), day=int(day)))       
+
 #This is to convert everything into an array and to check the frequency
 s1 = [float(x) for x in pfile['gs_ratio']]
 s2 = [float(x) for x in pfile['go_ratio']]
-#s3 = [float(x) for x in afile['ma10_h40']]
-#s4 = [float(x) for x in afile['ma10_l40']]
+s3 = [float(x) for x in test4['ma6tax']]
+s4 = [float(x) for x in test4['ma6spval']]
 #s5 = [float(x) for x in afile['Ind_Close']]
 #s6 = [float(x) for x in afile['ma10_150_delta']]
 #s7 = [float(x) for x in afile['ma10_40_delta']]
 #s8 = [float(x) for x in afile['ma10_ad10']]
 s1=np.array(s1)
 s2=np.array(s2)
-#s3=np.array(s3)
-#s4=np.array(s4)
+s3=np.array(s3)
+s4=np.array(s4)
 #s5=np.array(s5)
 #s6=np.array(s6)
 #s7=np.array(s7)
@@ -72,13 +105,14 @@ s2=np.array(s2)
 dateplot=np.array(dateplot)
 print('s1 - ' + str(len(s1)))
 print('s2 - ' + str(len(s2)))
-#print('s3 - ' + str(len(s3)))
-#print('s4 - ' + str(len(s4)))
+print('s3 - ' + str(len(s3)))
+print('s4 - ' + str(len(s4)))
 #print('s5 - ' + str(len(s5)))
 #print('s6 - ' + str(len(s6)))
 #print('s7 - ' + str(len(s7)))
 #print('s8 - ' + str(len(s8)))
 print('dateplot - ' + str(len(dateplot)))
+print('dateplot2 - ' + str(len(dateplot2)))
 
 fig = plt.figure(figsize=(20,15))
 #As soon as graph1 is initialized, everything below the block is included until another graph is initialized
@@ -109,9 +143,44 @@ graph1.xaxis.set_ticks(np.arange(start, end, 3.0 *365))
 graph1.plot()
 graph1.legend(loc='upper left')
 
+#Build the graph on two axes
 
 
-       
+fig = plt.figure(figsize=(20,15))
+#As soon as graph1 is initialized, everything below the block is included until another graph is initialized
+graph1 = fig.add_subplot(411)
+graph1.tick_params('y', colors='b')
+graph1.plot(dateplot,s1,'b:', linewidth=2.0, label='G/S Ratio')
+#graph1.plot(dateplot,s2,'k:', linewidth=2.0, dashes=(5,10), label='150dayL')
+start, end = graph1.get_xlim()
+graph2 = graph1.twinx() #this creates the same x axis with an independent y
+graph2.tick_params('y', colors='r')
+#graph2.plot(dateplot,s2,'r-', linewidth=2.0, label='G/O Ratio')
+graph2.plot(dateplot,s2,'r-', linewidth=1.0)
+graph2.plot(dateplot,s2,'r:', linewidth=2.0, label='G/O Ratio')
+graph2.xaxis.set_ticks(np.arange(start, end, 4.0 *365))
+plt.show
+
+
+fig = plt.figure(figsize=(20,15))
+#As soon as graph1 is initialized, everything below the block is included until another graph is initialized
+graph1 = fig.add_subplot(411)
+graph1.tick_params('y', colors='b')
+graph1.plot(dateplot2,s3,'b:', linewidth=2.0, label='G/S Ratio')
+graph1.set_ylabel('US Tax Receipts')
+plt.legend(loc='best')
+#graph1.plot(dateplot,s2,'k:', linewidth=2.0, dashes=(5,10), label='150dayL')
+start, end = graph1.get_xlim()
+graph2 = graph1.twinx() #this creates the same x axis with an independent y
+graph2.tick_params('y', colors='r')
+graph2.set_ylabel('S&P500')
+#graph2.plot(dateplot,s2,'r-', linewidth=2.0, label='G/O Ratio')
+graph2.plot(dateplot2,s4,'r-', linewidth=1.0)
+graph2.plot(dateplot2,s4,'r:', linewidth=2.0, label='G/O Ratio')
+plt.legend(loc='best')
+graph2.xaxis.set_ticks(np.arange(start, end, 4.0 *365))
+plt.show
+
 
         
         
@@ -155,7 +224,7 @@ graph1.axes.xaxis.set_ticklabels([])
 graph2 = graph1.twinx() #this creates the same x axis with an independent y
 graph2.tick_params('y', colors='r')
 graph2.set_ylabel('Index Close')
-graph2.plot(dateplot,s5,'r-', linewidth=1.0)
+graph2.plot(dateplot,s2,'r-', linewidth=1.0)
 plt.show
 
 graph3 = fig.add_subplot(412)
