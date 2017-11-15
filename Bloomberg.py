1628b6e2e746d8eec890cfc527bd17e9b94ceb12
stealthydonut <kinger6688@gmail.com>
2017-11-15 16:28:57 -0500

    Update Bloomberg.py

--- Bloomberg.py
+++ Bloomberg.py
@@ -15,3 +15,8 @@ labels=['time','price']
 df = pd.DataFrame(datapoints, columns=labels)
 labels=['time','price']
 df['UNIXTIME'] = pd.to_datetime(df['time'], unit='ms')
+#identify max/min value/open/close
+dfmax=df['price'].max()
+dfmin=df['price'].min()
+dfmaxtime=df['UNIXTIME'].max()
+dfmintime=df['UNIXTIME'].min()
