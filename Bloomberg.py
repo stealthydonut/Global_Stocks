cd1ea1b4ab3c46fc0734111d0f68740835aff7a1
stealthydonut <kinger6688@gmail.com>
2017-11-15 21:32:17 -0500

    Update Bloomberg.py

--- Bloomberg.py
+++ Bloomberg.py
@@ -7,7 +7,6 @@ import datetime
 ###########################################
 #Extract minute by minute bloomberg prices#
 ###########################################
-
 htmltext = urllib.urlopen(" http://www.bloomberg.com/markets/chart/data/1D/AAPL:US")           
 data = json.load(htmltext)
 datapoints = data["data_values"]
@@ -15,11 +14,32 @@ labels=['time','price']
 df = pd.DataFrame(datapoints, columns=labels)
 labels=['time','price']
 df['UNIXTIME'] = pd.to_datetime(df['time'], unit='ms')
-#identify max/min value/open/close
+df['ind']=df.index
+df['TICKER']='AAPL:US'
+#Get the open, close, high, low  
 dfmax=df['price'].max()
 dfmin=df['price'].min()
 dfmaxtime=df['UNIXTIME'].max()
-dfmintime=df['UNIXTIME'].min()
+dfmintime=df['UNIXTIME'].min() 
+#Identify the 
+high=df[df['price']==dfmax]
+low=df[df['price']==dfmin]
+#Open Price
+openpr=df[df['UNIXTIME']==dfmaxtime]
+openpr['OPEN']=openpr['price']
+openpr['DATE'] = openpr['UNIXTIME'].dt.strftime("%x")
+del openpr['time']
+del openpr['ind']
+del openpr['price']
+del openpr['UNIXTIME']
+#Close Price
+close=df[df['UNIXTIME']==dfmaxtime]
+close['CLOSE']=close['price']
+close['DATE'] = close['UNIXTIME'].dt.strftime("%x")
+del close['time']
+del close['ind']
+del close['price']
+del close['UNIXTIME']
+opcl=pd.merge(close, openpr, how='outer', left_on=('TICKER','DATE'), right_on=('TICKER','DATE'))
 
-
-df2['HIGH']=df['price'].idxmax()
+print opcl.dtypes
