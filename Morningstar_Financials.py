6cf6c4dcac908a7cfbcd3e3a2b8e7fb943e0f760
stealthydonut <kinger6688@gmail.com>
2017-11-21 15:17:24 -0500

    Update Morningstar_Financials.py

--- Morningstar_Financials.py
+++ Morningstar_Financials.py
@@ -7,6 +7,10 @@ link="http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB"
 f = urllib.urlopen(link)
 
 #Get the Financials
-daily_prices = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=3, nrows=15)
-print daily_prices
-'measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'
+financials = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=3, nrows=15)
+link="http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB"
+f = urllib.urlopen(link)
+balance_sheet = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=74, nrows=20)
+
+
+
