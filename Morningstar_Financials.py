d8e244ac18b701e3bdee5c3a752b8ae9a74a74e9
stealthydonut <kinger6688@gmail.com>
2017-11-22 10:35:45 -0500

    Update Morningstar_Financials.py

--- Morningstar_Financials.py
+++ Morningstar_Financials.py
@@ -40,12 +40,16 @@ tickerlist = [x.strip(' ') for x in df2]
 #for canadian stocks http://financials.morningstar.com/ajax/exportKR2CSV.html?t=GOLD&region=CAN&culture=en-CA
 link="http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB"
 f = urllib.urlopen(link)
-
 #Get the Financials
 financials = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=3, nrows=15)
+#Get the Balance Sheet Items
 link="http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB"
 f = urllib.urlopen(link)
 balance_sheet = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=74, nrows=20)
+#Liquidity Financial Health
+link="http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB"
+f = urllib.urlopen(link)
+balance_sheet = pd.read_csv(f, sep=",",names=['measure','2007-12','2008-12','2009-12','2010-12','2011-12','2012-12','2013-12','2014-12','2015-12','2016-12','TTM'], skiprows=96, nrows=4)
 
 
 
